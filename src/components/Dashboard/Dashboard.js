d3389df1016a115cea1f244ec12d71c191e01a0e
Arpan Barua <arpanctg7@gmail.com>
2022-02-26 12:40:17 -0500

    updated

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
-// import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
--- src/components/ManageProduct/ManageProduct.js
+++ src/components/ManageProduct/ManageProduct.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-// import { Link } from "react-router-dom";
 import { PageContext } from "../../App";
 import Sidebar from "../Sidebar/Sidebar";
 import "./ManageProduct.css";
